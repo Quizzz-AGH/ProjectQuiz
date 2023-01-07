aee249329e8f3d75f24f6015d566b5145a6f858e
Gasiorek, Piotr <piotrgasiorek773@gmail.com>
2023-01-07 22:58:58 +0100

    dodałem autoryzacje dla adminów do UserRoutera dodalem updatePassword

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers, updateUser, deleteUser } = require("../controllers/usersController");
+const { getAllUsers, updateUser, deleteUser, updateUserPassword } = require("../controllers/usersController");
+const { authenticateUser, authorizePermissions } = require("../middleware/authentication");
 
-router.route("/").get(getAllUsers);
-router.route("/:accountId").patch(updateUser).delete(deleteUser);
+router.route("/").get(authenticateUser, authorizePermissions("admin"), getAllUsers);
+router.route("/updatePassword").patch(authenticateUser, authorizePermissions("admin", "guest"), updateUserPassword);
+router
+  .route("/:accountId")
+  .patch(authenticateUser, authorizePermissions("admin"), updateUser)
+  .delete(authenticateUser, authorizePermissions("admin"), deleteUser);
 
 module.exports = router;
